0b239dc55942da52fe07c0c4ce5d4dbba709488a
Minimoocodes <jaemin.shim14@gmail.com>
2024-07-22 15:52:32 +0300

    create custom hook to fetch games

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -3,6 +3,6 @@ import axios from 'axios';
 export default axios.create({
   baseURL: 'https://api.rawg.io/api',
   params: {
-    key: '83e92108eea6403e9350ab68ecc26883',
+    key: import.meta.env.VITE_API_KEY,
   }
 })
